6d5049f482d6308c54f5d2a0bd4f8ba5138eeb52
Chaemyoung(Tad) Yoon <chaemyoung2015@gmail.com>
2024-03-19 09:23:56 -0700

    Add js code to save the change of profile

--- scripts/profile.js
+++ scripts/profile.js
@@ -40,3 +40,23 @@ function editUserInfo() {
     //Enable the form fields
     document.getElementById('personalInfoFields').disabled = false;
  }
+
+ function saveUserInfo() {
+    //a) get user entered values
+    userName = document.getElementById('nameInput').value;       //get the value of the field with id="nameInput"
+    userSchool = document.getElementById('schoolInput').value;     //get the value of the field with id="schoolInput"
+    userCity = document.getElementById('cityInput').value;       //get the value of the field with id="cityInput"
+
+    //b) update user's document in Firestore
+    currentUser.update({
+        name: userName,
+        school: userSchool,
+        city: userCity
+    })
+    .then(() => {
+        console.log("Document successfully updated!");
+    })
+
+    //c) disable edit 
+    document.getElementById('personalInfoFields').disabled = true;
+}
